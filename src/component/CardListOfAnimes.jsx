7b5e043e45af51b2e25eb9f9f174e6fcbf0253c4
lautaro <you@example.com>
2023-08-08 16:49:37 -0300

    funciona

--- src/component/CardListOfAnimes.jsx
+++ src/component/CardListOfAnimes.jsx
@@ -1,12 +1,15 @@
 import { CardAnimeAndManga } from "./CardAnimeAndManga";
+import { useSearchMangaOrAnimeRegex } from "../hooks/useSearchMangaOrAnimeRegex";
+import { useTop } from "../hooks/useTopAnime";
 
-export function CardListOfAnimes({ animeArray, pagination }) {
+export default function CardListOfAnimes() {
+	const { isAnime } = useSearchMangaOrAnimeRegex();
+	const { top } = useTop({ types: isAnime });
+	const { data, pagination } = top;
+
 	return (
 		<section id="top_anime_conteiner">
-			<CardAnimeAndManga
-        animeArray={animeArray}
-        pagination={pagination}
-      />
+			<CardAnimeAndManga animeArray={data} pagination={pagination} />
 		</section>
 	);
 }
--- src/component/TopAnimeCard.jsx
+++ src/component/TopAnimeCard.jsx
@@ -1,20 +1,17 @@
 import { Slice } from "./Slice";
-import './css/image.css'
+import "./css/image.css";
 
 function TopAnime({ animeArray }) {
 	return (
 		<Slice>
-			{animeArray?.slice(0, 6).map((data) => {
+			{animeArray?.data?.slice(0, 6).map((data) => {
 				const id = data.mal_id;
 				const { images, title } = data;
 				const { webp } = images;
 				const { image_url } = webp;
-				return <img
-          className="imageCover"
-          key={id}
-          src={image_url}
-          alt={title}
-        />;
+				return (
+					<img className="imageCover" key={id} src={image_url} alt={title} />
+				);
 			})}
 		</Slice>
 	);
@@ -28,7 +25,7 @@ function AnimeCharacter({ animeArray }) {
 				const id = character.mal_id;
 				return (
 					<img
-            className="imageCover"
+						className="imageCover"
 						key={id}
 						src={character.images.webp.image_url}
 						alt={`Character: ${character.name}`}
