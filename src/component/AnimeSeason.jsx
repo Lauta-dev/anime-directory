2334baacf7f0ac2a645d994886bf8a6e3a6519e3
lautaro <you@example.com>
2023-08-10 00:05:58 -0300

    asds

--- src/component/AnimeSeason.jsx
+++ src/component/AnimeSeason.jsx
@@ -1,19 +1,14 @@
 import { getFullSeasonNow } from "../logic/getFullSeasonNow";
-import { CardListOfAnimes } from "./CardListOfAnimes";
+import CardListOfAnimes from "./CardListOfAnimes";
 
 export default function AnimeSeason() {
-	const { seasonFullNow } = getFullSeasonNow({ limit: 25, page: 1 });
-
-	const count = seasonFullNow?.pagination?.items?.count;
-	const total = seasonFullNow?.pagination?.items?.total;
-
-	const data = seasonFullNow?.data;
+	const { seasonFullNow } = getFullSeasonNow({ limit: 25 });
+	const { data, pagination } = seasonFullNow;
 
 	return (
 		<>
-			{count ? <h2>{`${count}/${total}`}</h2> : <p>Cargando</p>}
 			{data?.length ? (
-				<CardListOfAnimes animeArray={data} />
+				<CardListOfAnimes pagination={pagination} animeArray={data} />
 			) : (
 				<h2>Cargando</h2>
 			)}
--- src/logic/getFullSeasonNow.js
+++ src/logic/getFullSeasonNow.js
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SEASON_ANIME_NOW } from "../const";
+import { desactiveNSFWContext } from "../context/desactiveNSFW";
 
-export const getFullSeasonNow = ({ limit, page }) => {
+export const getFullSeasonNow = ({ limit }) => {
 	const [seasonFullNow, setSeasonFullNow] = useState({});
+	const { nsfw } = useContext(desactiveNSFWContext);
+
+	const page = nsfw.page;
 
 	useEffect(() => {
 		fetch(SEASON_ANIME_NOW({ limit, page }))
@@ -11,7 +15,7 @@ export const getFullSeasonNow = ({ limit, page }) => {
 			.catch((r) => {
 				throw new Error(`Error al obtener la temporada de anime ${r}`);
 			});
-	}, []);
+	}, [nsfw]);
 
 	return { seasonFullNow };
 };
